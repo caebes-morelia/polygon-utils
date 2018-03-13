b65ece4ee02ceab93ea7eed911576ec92596a81c
Yair Díaz <yaird0910@gmail.com>
2018-03-12 21:32:31 -0600

    detalles corregidos

--- src/polygon.js
+++ src/polygon.js
@@ -1,11 +1,11 @@
-// var points_pologono = [][];
+
 const polygonPoints = [[1, 3], [3, 3], [4, 4], [6, 4], [7, 3], [8, 5], [5, 7], [4, 5], [2, 6]]; // points that make up the polygon
 const orderXaxis = polygonPoints.slice();
 orderXaxis.sort(); // are the points of the polygon but ordered from lowest to highest on the X axis
 
 const liminx = orderXaxis[0][0];
 const limaxx = orderXaxis[orderXaxis.length - 1][0];
-let liminy = 1000;
+let [liminy] = 1000;
 let limaxy = -1000;
 
 for (let c = 0; c < polygonPoints.length; c += 1) {
@@ -54,12 +54,11 @@ rl.question('enter the X coordinate of the point: ', (answer) => {
 
       for (let axisx; i < orderXaxis.length; i += 1) {
         // the point is traveled along the X axis to see how many borders it crosses
-        axisx = orderXaxis[i][0]; // console.log('axisx: '+axisx);
+        axisx = orderXaxis[i][0];
         for (let point = 0; point < polygonPoints.length; point += 1) {
           // look in the polygon points array a point that is the same distance from X as the point travel
           if (polygonPoints[point][0] === axisx) {
             // if find a point of the polygon that is at the same height on the X axis as the point
-            // console.log('vertice:'+polygonPoints[point]);
             if (point + 1 === polygonPoints.length) {
               nextpoint = 0;
             } else {
@@ -69,7 +68,6 @@ rl.question('enter the X coordinate of the point: ', (answer) => {
             if (polygonPoints[point][1] >= y) {
               // if the polygon's found point is higher on the Y axis
               if (polygonPoints[point - 1][1] <= y && polygonPoints[point - 1][0] <= axisx) {
-                // console.log('vertice ant:'+polygonPoints[point-1]);
                 if (borders === 0 && x <= polygonPoints[point][0] && x > polygonPoints[point - 1][0]) {
                   lx = Math.abs(polygonPoints[point][0] - polygonPoints[point - 1][0]);
                   ly = Math.abs(polygonPoints[point][1] - polygonPoints[point - 1][1]);
@@ -86,18 +84,15 @@ rl.question('enter the X coordinate of the point: ', (answer) => {
                 borders += 1;
                 console.log('1');
               } else if (polygonPoints[nextpoint][1] <= y && polygonPoints[nextpoint][0] <= axisx) {
-                // if the previous or next point of the point found is below the point on the Y axis and behind the X axis of the current point and in front of X
+                // if the previous or next point of the point found is below the point on the Y axis
+                // and behind the X axis of the current point and in front of X
                 if (borders === 0 && x <= polygonPoints[point][0] && x > polygonPoints[nextpoint][0]) {
                   lx = Math.abs(polygonPoints[point][0] - polygonPoints[nextpoint][0]);
                   ly = Math.abs(polygonPoints[point][1] - polygonPoints[nextpoint][1]);
-                  // var h = Math.sqrt(Math.pow(lx, 2) + Math.pow(ly, 2));
-                  // var angle = Math.asin((ly*Math.sin(Math.PI/2))/h);
                   angle = Math.atan(ly / lx) * (180 / Math.PI);
                   console.log(`angleulo2: ${angle}`);
                   lx = Math.abs(polygonPoints[point][0] - x);
                   ly = Math.abs(polygonPoints[point][1] - y);
-                  // h = Math.sqrt(Math.pow(lx, 2) + Math.pow(ly, 2));
-                  // var anglePoint = Math.asin((ly*Math.sin(Math.PI/2))/h);
                   anglePoint = Math.atan(ly / lx) * (180 / Math.PI);
                   console.log(`angleulo point2: ${anglePoint}`);
                   if (anglePoint > angle) {

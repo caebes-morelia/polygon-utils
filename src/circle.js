cdeb1e9c2fbd337390e264b081d772f9098e1e2d
Yair Díaz <yaird0910@gmail.com>
2018-03-13 18:29:58 -0600

    cambios para empezar a convertir a funciones

--- src/circle.js
+++ src/circle.js
@@ -1,48 +1,47 @@
-var circulo = [19.729378, -101.175365];
-var r = 3;
-var lat1 = circulo[0];
-var lon1 = circulo[1];
-var lat2;
-var lon2;
-var radio = 6371; //radio de la tierra en km
-// radio ecuatorial=6378km	radio polar=6357km	radio equivolumen=6371km
+const circle = [19.729378, -101.175365];
+const r = 3;
+const lat1 = circle[0];
+const lon1 = circle[1];
+let lat2;
+let lon2;
+const radius = 6371; // radius of the earth in km
+// equatorial radius=6378km  polar radius=6357km  equivolume radius=6371km
 
 const readline = require('readline');
+
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
-rl.question('ingresa la latitud del punto: ', function(respuesta1) {
-  if (isNaN(respuesta1) || respuesta1 < -90 || respuesta1 > 90) console.log('no es una coordenada valida');
+rl.question('enter the latitude of the point: ', (answer1) => {
+  if (Number.isNaN(answer1) || answer1 < -90 || answer1 > 90) console.log('it is not a valid coordinate');
   else {
-    //x = 19.702562;
-    lat2 = respuesta1;
-    console.log('lat=' + lat2);
+    lat2 = answer1;
+    console.log(`lat=${lat2}`);
   }
-  rl.question('ingresa la longitud del punto: ', function(respuesta2) {
-    if (isNaN(respuesta2) || respuesta2 < -180 || respuesta2 > 180) console.log('no es una coordenada valida');
+  rl.question('enter the longitude of the point: ', (answer2) => {
+    if (Number.isNaN(answer2) || answer2 < -180 || answer2 > 180) console.log('it is not a valid coordinate');
     else {
-      //y = -101.191617;
-      lon2 = respuesta2;
-      console.log('lon=' + lon2);
+      lon2 = answer2;
+      console.log(`lon=${lon2}`);
     }
 
-    ////////////formula de Haversine
-    var dLat = (lat2 - lat1) * Math.PI / 180;
-    var dLon = (lon2 - lon1) * Math.PI / 180;
-    var a =
-      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
-    var c = 2 * Math.asin(Math.sqrt(a));
-    var distancia = radio * c;
-    console.log('distancia del centro del circulo al punto: ' + distancia);
+    // //////////Haversine formula
+    const dLat = (lat2 - lat1) * (Math.PI / 180);
+    const dLon = (lon2 - lon1) * (Math.PI / 180);
+    const a =
+      (Math.sin(dLat / 2) * Math.sin(dLat / 2)) +
+      (Math.cos(lat1 * (Math.PI / 180)) * Math.cos(lat2 * (Math.PI / 180)) * Math.sin(dLon / 2) * Math.sin(dLon / 2));
+    const c = 2 * Math.asin(Math.sqrt(a));
+    const distance = radius * c;
+    console.log(`distance from the center of the circle to the point: ${distance}`);
 
-    if (distancia <= r) {
-      //si la distancia del centro del circulo al punto es menor o igual al radio del circulo entonces el punto esta dentro
-      console.log('el punto esta dentro del circulo');
+    if (distance <= r) {
+      // if the distance from the center of the circle to the point is less than or equal to the radius of the circle, the point is inside
+      console.log('the point is inside the circle');
     } else {
-      console.log('el punto esta fuera del circulo');
+      console.log('the point is outside the circle');
     }
     rl.close();
   });
